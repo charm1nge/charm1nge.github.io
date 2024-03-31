d2c1ae18bfaa965717a43dc0013c70268e9f322d
plutoguy777 <165219360+plutoguy777@users.noreply.github.com>
2024-03-31 21:10:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,10 @@ function createApp(name, icon, url) {
 }
 
 
+createApp('minecraft-java-edition', 'app-icons/minecraft-java-edition.webp', "/games/minecraft-java.html");
 createApp('subway-surfers', 'app-icons/subway-surfers.webp', "/games/subway-surfers.html");
+createApp('temple-run-2', 'app-icons/temple-run-2.webp', "/games/temple-run-2.html");
+createApp('stickman-hook', 'app-icons/stickman-hook.webp', "/games/stickman-hook.html");
 createApp('krunker', 'app-icons/krunker.webp', "/games/krunker.html");
 createApp('friday-night-funkin', 'app-icons/friday-night-funkin.webp', "/games/friday-night-funkin.html");
 createApp('hexgl', 'app-icons/hexgl.webp', "/games/hexgl.html");
@@ -33,10 +36,7 @@ createApp('snowball-io', 'app-icons/snowball-io.webp', "/games/snowball-io.html"
 createApp('baldi-basics', 'app-icons/baldi-basics.webp', "/games/baldi-basics.html");
 createApp('paper-io', 'app-icons/paper-io-4.webp', "/games/paper-io.html");
 createApp('smash-karts', 'app-icons/smash-karts.webp', "/games/smash-karts.html");
-createApp('stickman-hook', 'app-icons/stickman-hook.webp', "/games/stickman-hook.html");
 createApp('time-shooter-2', 'app-icons/time-shooter-2.webp', "/games/time-shooter-2.html");
-createApp('temple-run-2', 'app-icons/temple-run-2.webp', "/games/temple-run-2.html");
 createApp('kirka', 'app-icons/kirka.webp', "/games/kirka.html");
-createApp('minecraft-java-edition', 'app-icons/minecraft-java-edition.webp', "/games/minecraft-java.html");
 createApp('tall-io', 'app-icons/tall-io.webp', "/games/tall-io.html");
 createApp('1v1-lol', 'app-icons/1v1lol.webp', "/games/1v1lol.html"); 
