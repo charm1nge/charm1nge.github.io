846250f578d29b6d049e0b51956b3f1eb23b3568
plutoguy <123932868+plutoguy@users.noreply.github.com>
2024-10-20 16:48:03 +0100

    Update player.js

--- games/player.js
+++ games/player.js
@@ -1,7 +1,20 @@
 var gameplayer = document.getElementById("y");
 var button = document.getElementById("papa");
 
-button.addEventListener('click',fullscreen)
+window.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.shiftKey && event.key === 'I') {
+        event.preventDefault();
+    }
+});
+
+window.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.shiftKey && event.key === 'J') {
+        event.preventDefault();
+    }
+});
+
+
+button.addEventListener('click',fullscreen);
 
 
 function fullscreen()
