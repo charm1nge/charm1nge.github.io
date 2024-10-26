a76610186a78f1a89ae36a8f531fee72f9709d86
plutoguy <123932868+plutoguy@users.noreply.github.com>
2024-10-27 00:32:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,11 +21,13 @@ function createApp(name, icon, url) {
     image.style.transition = "transform 0.3s ease-in-out";
 
     image.addEventListener('mouseover', () => {
+        image.style.zIndex = "-9999";
         image.style.transform = "scale(1.25)";
     });
 
     image.addEventListener('mouseout', () => {
         image.style.transform = "scale(1)";
+        image.style.zIndex = "-9999";
     });
     
     image.style.display = "block";
